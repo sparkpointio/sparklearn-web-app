8d17e9bc84c391c2f1193e5c41abc40b383b44ff
John Paul Batusan <johnpaul.batusa@bicol-u.edu.ph>
2020-09-03 17:06:00 +0800

    Make button text both lowercase

--- theme/moove/classes/output/core_renderer.php
+++ theme/moove/classes/output/core_renderer.php
@@ -326,7 +326,7 @@ class core_renderer extends \theme_boost\output\core_renderer {
         if (!isloggedin()) {
             $returnstr = '';
             if (!$loginpage) {
-                $returnstr .= "<a class='btn btn-login-top' href=\"$loginurl\">" . get_string('login') . "</a><a href='https://earn.sparkpoint.io/register' target='_blank'><button class='btn btn-login-top ml-1' style='border-radius:16px;'>REGISTER</BUTTON></a>";
+                $returnstr .= "<a class='btn btn-login-top' href=\"$loginurl\">" . get_string('login') . "</a><a href='https://earn.sparkpoint.io/register' target='_blank'><button class='btn btn-login-top ml-1' style='border-radius:16px;'>Register</BUTTON></a>";
             }
 
             $theme = theme_config::load('moove');
