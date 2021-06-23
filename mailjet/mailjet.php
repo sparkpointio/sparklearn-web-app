fbc1a939d3ad626541224364b48e4c682ba98381
Christian Salvadora <christianrosales.salvadora@bicol-u.edu.ph>
2021-06-23 15:32:15 +0800

    Fix email origin

--- mailjet/mailjet.php
+++ mailjet/mailjet.php
@@ -1,25 +1,22 @@
 <?php
+include('../config.php');
 include("./php-mailjet-v3-simple.class.php");
 //include('php-mailjet.class-mailjet-0.1.php');
 
-$apiKey = '67aa883ca6db9450ccca857983ba983b';
-$secretKey = '740ea5b02c92252385278f399be9bf07';
-
-$mj = new Mailjet($apiKey, $secretKey);
+$mj = new Mailjet($CFG->mailjet_user, $CFG->mailjet_key);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['fullname'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    echo('name:'.$name.' email:'.$email.' message:'.$message);
     $params = array(
         'method' => 'POST',
-        'from' => $email,
+        'from' => 'loboysalvadora.26@gmail.com',
         'to' => 'christianrosales.salvadora@bicol-u.edu.ph',
         'subject' => 'Connect with Us',
-        'text' => $message
+        'text' => 'Fullname: '.$name.'\r\nEmail: '.$email.'\r\n'.$message
     );
     
     $result = $mj->sendEmail($params);
-
+    // header('Location: '.$CFG->wwwroot);
 }
 ?>
