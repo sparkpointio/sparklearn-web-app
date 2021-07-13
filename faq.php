c1ce7e165c15b9cb922152ed0a7205bc0c8eaf82
Pseudoman21 <johnpaul.batusa@bicol-u.edu.ph>
2021-07-13 10:09:41 +0800

    Fix faq layout

--- faq.php
+++ faq.php
@@ -3,7 +3,7 @@
 require_once('config.php');
 
 // $PAGE->set_context(get_system_context());
-// $PAGE->set_pagelayout('faq');
+$PAGE->set_pagelayout('faq');
 // $PAGE->set_title("FAQs");
 // $PAGE->set_heading("Frequently Asked Question");
 $PAGE->set_url($CFG->wwwroot.'/faq.php');
@@ -20,7 +20,7 @@ $templatecontext = [
     'secondguideimage' => $OUTPUT->image_url('secondguideimage', 'theme'),
     'thirdguideimage' => $OUTPUT->image_url('thirdguideimage', 'theme'),
 ];
-echo $OUTPUT->render_from_template('theme_moove/faq',$templatecontext);
+// echo $OUTPUT->render_from_template('theme_moove/faq',$templatecontext);
 // echo $OUTPUT->header();
 
 ?>
